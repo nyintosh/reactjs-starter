8b0363eaec4217ca3bda9f7bf8e79e5c8289a247
Nyi Nyi Htun <nyintosh@gmail.com>
2022-04-13 11:47:38 +0630

    Updated to latest version, added folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
 
 const App: React.FC = () => {
 	return (
-		<Routes>
-			<Route
-				path={'/'}
-				element={
-					<main
-						className={`absolute inset-0 grid place-items-center bg-slate-900`}
-					>
-						<h1
-							className={`text-[10vw] font-semibold text-gray-200`}
-						>
-							Hello React.JS!
-						</h1>
-					</main>
-				}
-			/>
-		</Routes>
+		<div className={`absolute inset-0 grid place-items-center bg-slate-900`}>
+			<p className={`text-[10vw] font-semibold text-gray-200`}>
+				<span>Hello, Alien!</span>
+			</p>
+		</div>
 	)
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-
 import App from './App'
-import './main.css'
+import './index.css'
 
-const $app = document.querySelector('#root')
+const root = createRoot(document.getElementById('app') as HTMLElement)
 
-ReactDOM.render(
+root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>,
-	$app
+	</React.StrictMode>
 )
